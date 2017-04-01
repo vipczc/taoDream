61a0b1182be7023c60e463f34e8b8ae5172290a9
kevin <kevin@vipzcc.com>
2017-04-01 14:28:26 +0800

    更新商家路由 更新商家模块

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-    <h1>111</h1>
-  </div>
+<div id="app">
+  <router-view></router-view>
+
+</div>
 </template>
 
 <script>
@@ -13,7 +13,7 @@ export default {
 
 <style>
 #app {
-  font-family: '微软雅黑','Avenir', Helvetica, Arial, sans-serif;
+  font-family: '微软雅黑', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,15 @@ import userList from '@/components/User/userList'
 import bIndex from '@/components/Business/index'
 import cIndex from '@/components/Clerk/index'
 
+// 商家模块
+import bIndexDream from '@/components/Business/modules/dream'//淘梦地带
+import bIndexExpenseRecord from '@/components/Business/modules/expenseRecord'//消费记录
+import bIndexWithdrawalsRecord from '@/components/Business/modules/withdrawalsRecord'//提现记录
+import bIndexConversionRecord from '@/components/Business/modules/conversionRecord'//兑换记录
+import bIndexTDRecord from '@/components/Business/modules/TDRecord'//淘豆流水
+import bIndexLimitRecord from '@/components/Business/modules/limitRecord'//额度流水
+
+
 Vue.use(Router)
 
 export default new Router({
@@ -43,7 +52,30 @@ export default new Router({
     {
       path: '/bussiness',
       name: 'bIndex',
-      component: bIndex
+      component: bIndex,
+      children:[{
+                    path:'',
+                    redirect:'dream'
+                  },{
+                    path:'dream',
+                    component:bIndexDream //淘梦地带
+                  },{
+                    path:'expenseRecord', //消费记录
+                    component:bIndexExpenseRecord
+                  },{
+                    path:'withdrawalsRecord', //提现记录
+                    component:bIndexWithdrawalsRecord
+                  },{
+                    path:'conversionRecord', //兑换记录
+                    component:bIndexConversionRecord
+                  },{
+                    path:'TDRecord', //淘豆流水
+                    component:bIndexTDRecord
+                  },{
+                    path:'limitRecord', //额度流水
+                    component:bIndexLimitRecord
+                  }]
+
     },
     // 业务员部分
     {
